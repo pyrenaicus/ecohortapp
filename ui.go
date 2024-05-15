cfb8ca9b1e5aa5aef3e559f3d3380cf4ddc4918d
Pau Guerrero Prado <pauguerrero@gmail.com>
2024-05-15 18:26:46 +0200

    makeUI plan

--- ui.go
+++ ui.go
@@ -1,1 +1,9 @@
 package main
+
+func (app *Config) makeUI() {
+	// Conectem amb API aemet i obtenim dades filtrades
+
+	//Incloure la info al contenidor
+
+	//Incloure el contenidor a la finestra principal
+}
