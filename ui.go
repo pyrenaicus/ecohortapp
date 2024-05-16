bfa5c5ce4b758538eb732cb018c7bb5da2501029
Pau Guerrero Prado <pauguerrero@gmail.com>
2024-05-16 16:29:17 +0200

    clima dades contenidor

--- ui.go
+++ ui.go
@@ -1,9 +1,17 @@
 package main
 
+import "fyne.io/fyne/v2/container"
+
 func (app *Config) makeUI() {
 	// Conectem amb API aemet i obtenim dades filtrades
 	precipitacio, tempMax, tempMin, humitat := app.getClimaText()
 	//Incloure la info al contenidor
+	climaDadesContenidor := container.NewGridWithColumns(4,
+		precipitacio,
+		tempMax,
+		tempMin,
+		humitat,
+	)
 
 	//Incloure el contenidor a la finestra principal
 }
