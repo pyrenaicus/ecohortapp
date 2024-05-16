a4b560182940af2554dceb1030452864385aac1b
Pau Guerrero Prado <pauguerrero@gmail.com>
2024-05-16 19:05:41 +0200

    repository package struct & interface

--- repository/repository.go
+++ repository/repository.go
@@ -1,1 +1,26 @@
 package repository
+
+import (
+	"errors"
+	"time"
+)
+
+var (
+	errorUpdatejant = errors.New("Ha fallat l'actulaització")
+	errorsBorrant   = errors.New("Ha fallat borrant")
+)
+
+type Repository interface {
+	Migrate() error
+	// Crear, Llegir un elment, llegir tots, Update, Borrar
+
+}
+
+type Registres struct {
+	ID           int64     `json:"id"`
+	Data         time.Time `json:"data_registre"`
+	Precipitacio int       `json:"precipitacio"`
+	TempMax      int       `json:"temp_max"`
+	TempMin      int       `json:"temp_min"`
+	Humitat      int       `json:"humitat"`
+}
