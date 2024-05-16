b29a572bc279034a76fcca66d7f5eb91b2e7f5e3
Pau Guerrero Prado <pauguerrero@gmail.com>
2024-05-16 17:32:57 +0200

    fix null pointer error

--- ui.go
+++ ui.go
@@ -1,6 +1,10 @@
 package main
 
-import "fyne.io/fyne/v2/container"
+import (
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
+)
 
 func (app *Config) makeUI() {
 	// Conectem amb API aemet i obtenim dades filtrades
@@ -15,10 +19,15 @@ func (app *Config) makeUI() {
 	//Incloure el contenidor a la finestra principal
 	app.ClimaDadesContainer = climaDadesContenidor
 
-	contenidorFinal := container.NewVBox(climaDadesContenidor)
-	app.MainWindow.SetContent(contenidorFinal)
 	// carrega del toolbar
 	barraEines := app.getToolbar(app.MainWindow)
-	contenidorFinal = container.NewVBox(climaDadesContenidor, barraEines)
+
+	// pestanyes
+	pestanyes := container.NewAppTabs(
+		container.NewTabItemWithIcon("Pronòstic", theme.HomeIcon(), canvas.NewText("cascascas", nil)),
+		container.NewTabItemWithIcon("Diari Meteorològic", theme.InfoIcon(), canvas.NewText("cascascas", nil)),
+	)
+
+	contenidorFinal := container.NewVBox(climaDadesContenidor, barraEines, pestanyes)
 	app.MainWindow.SetContent(contenidorFinal)
 }
